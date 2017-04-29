070355ec3b0a78715454dfef7b8f3f35b1607f9e
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-04-29 20:22:27 +0200

    movement yay

--- frontend/pacman.js
+++ frontend/pacman.js
@@ -13,9 +13,14 @@ var __extends = (this && this.__extends) || (function () {
 function onLoad() {
     var hb = 1; // half border width
     var ht = 4; // half tile width
-    var playerSpeed = 500;
+    var playerSpeed = 5;
     var ghostSpeed = playerSpeed;
-    var levelString = "\n############################\n#............##............#\n#.####.#####.##.####.#####.#\n#o####.#####.##.####.#####o#\n#.####.#####.##.####.#####.#\n#..........................#\n#.####.##.########.##.####.#\n#.####.##.########.##.####.#\n#......##....##....##......#\n######.##### ## #####.######\n     #.##### ## #####.#     \n     #.##    B     ##.#     \n     #.## ###--### ##.#     \n######.## #I P C # ##.######\n      .   #  R   #   .      \n######.## #      # ##.######\n     #.## ######## ##.#     \n     #.##          ##.#     \n     #.## ######## ##.#     \n######.## ######## ##.######\n#............##............#\n#.####.#####.##.#####.####.#\n#.####.#####.##.#####.####.#\n#o..##.......M .......##..o#\n###.##.##.########.##.##.###\n###.##.##.########.##.##.###\n#......##....##....##......#\n#.##########.##.##########.#\n#.##########.##.##########.#\n#..........................#\n############################";
+    var impassable = {
+        '#': true,
+        '-': true,
+        '-': true
+    };
+    var levelString = "\n############################\n#............##............#\n#.####.#####.##.####.#####.#\n#o####.#####.##.####.#####o#\n#.####.#####.##.####.#####.#\n#..........................#\n#.####.##.########.##.####.#\n#.####.##.########.##.####.#\n#......##....##....##......#\n######.##### ## #####.######\n     #.##### ## #####.#     \n     #.##    B     ##.#     \n     #.## ###--### ##.#     \n######.## #I P C # ##.######\nTODO# .   #  R   #   . #TODO\n######.## #      # ##.######\n     #.## ######## ##.#     \n     #.##          ##.#     \n     #.## ######## ##.#     \n######.## ######## ##.######\n#............##............#\n#.####.#####.##.#####.####.#\n#.####.#####.##.#####.####.#\n#o..##.......M .......##..o#\n###.##.##.########.##.##.###\n###.##.##.########.##.##.###\n#......##....##....##......#\n#.##########.##.##########.#\n#.##########.##.##########.#\n#..........................#\n############################";
     var Level = (function () {
         function Level(levelString) {
             this._data = _.chain(levelString).
@@ -58,6 +63,12 @@ function onLoad() {
         Vec2.prototype.mul = function (factor) {
             return new Vec2(this.x * factor, this.y * factor);
         };
+        Vec2.prototype.round = function () {
+            return new Vec2(Math.round(this.x), Math.round(this.y));
+        };
+        Vec2.prototype.floor = function () {
+            return new Vec2(Math.floor(this.x), Math.floor(this.y));
+        };
         return Vec2;
     }());
     var zero = new Vec2(0, 0);
@@ -79,14 +90,34 @@ function onLoad() {
             this.wantDir = zero;
             this.progress = 0;
         }
-        Mover.prototype._tryChangeDir = function (level) {
+        Mover.prototype._passable = function (level, dir) {
+            return !impassable[level.get(this.pos.add(dir))];
         };
-        Mover.prototype.update = function (level, deltaT) {
-            if (this.dir.zero() && !this.wantDir.zero()) {
+        Mover.prototype.drawPos = function () {
+            return this.pos.add(this.dir.mul(this.progress)).mul(2 * ht);
+        };
+        Mover.prototype.update = function (level, deltaTMS) {
+            if (this.dir.zero() && !this.wantDir.zero() && this._passable(level, this.wantDir)) {
                 this.dir = this.wantDir;
+                this.wantDir = zero;
             }
             if (this.dir.zero()) {
                 return;
+            }
+            this.progress += deltaTMS * this.speed / 1000;
+            while (this.progress >= 1) {
+                this.progress -= 1;
+                this.pos = this.pos.add(this.dir);
+                if (!this.wantDir.zero() && this._passable(level, this.wantDir)) {
+                    // change direction if desired & valid
+                    this.dir = this.wantDir;
+                    this.wantDir = zero;
+                }
+                else if (!this._passable(level, this.dir)) {
+                    // stop moving upon hitting wall
+                    this.dir = zero;
+                    this.progress = 0;
+                }
             }
         };
         return Mover;
@@ -100,19 +131,62 @@ function onLoad() {
     }(Mover));
     var Ghost = (function (_super) {
         __extends(Ghost, _super);
-        function Ghost(pos) {
-            return _super.call(this, pos, ghostSpeed) || this;
+        function Ghost(pos, color) {
+            var _this = _super.call(this, pos, ghostSpeed) || this;
+            _this.color = color;
+            return _this;
         }
         return Ghost;
     }(Mover));
     var Game = (function () {
         function Game(levelString) {
             this.level = new Level(levelString);
-            for (var y = 0; y < this.level.height(); y++) {
-                for (var x = 0; x < this.level.width(y); x++) {
+            this.ghosts = {};
+            for (var pos = new Vec2(0, 0); pos.y < this.level.height(); pos.y++) {
+                for (pos.x = 0; pos.x < this.level.width(pos.y); pos.x++) {
+                    var tile = this.level.get(pos);
+                    if (tile == 'M') {
+                        this.player = new Player(new Vec2(pos.x, pos.y)); // copy because I'm naughty and mutate
+                    }
+                    else if (tile == 'I') {
+                        this.ghosts["inky"] = new Ghost(new Vec2(pos.x, pos.y), "cyan");
+                    }
+                    else if (tile == 'B') {
+                        this.ghosts["blinky"] = new Ghost(new Vec2(pos.x, pos.y), "red");
+                    }
+                    else if (tile == 'P') {
+                        this.ghosts["pinky"] = new Ghost(new Vec2(pos.x, pos.y), "pink");
+                    }
+                    else if (tile == 'C') {
+                        this.ghosts["clyde"] = new Ghost(new Vec2(pos.x, pos.y), "orange");
+                    }
                 }
             }
         }
+        Game.prototype.playerMove = function (dir) {
+            console.log("move " + dir);
+            var wantDir = dirs[dir];
+            if (wantDir != null) {
+                this.player.wantDir = wantDir;
+            }
+        };
+        Game.prototype.ghostMove = function (who, dir) {
+            var ghost = this.ghosts[who];
+            if (ghost == null) {
+                return;
+            }
+            var wantDir = dirs[dir];
+            if (wantDir != null) {
+                ghost.wantDir = wantDir;
+            }
+        };
+        Game.prototype.update = function (deltaTMS) {
+            var that = this;
+            this.player.update(this.level, deltaTMS);
+            _.forEach(this.ghosts, function (ghost) {
+                ghost.update(that.level, deltaTMS);
+            });
+        };
         return Game;
     }());
     var game = new Game(levelString);
@@ -125,6 +199,9 @@ function onLoad() {
         }
     }
     function drawLevel(ctx, level) {
+        ctx.save();
+        ctx.fillStyle = "blue";
+        ctx.strokeStyle = "blue";
         var pos = new Vec2(0, 0);
         for (; pos.y < level.height(); pos.y++) {
             for (pos.x = 0; pos.x < level.width(pos.y); pos.x++) {
@@ -135,8 +212,12 @@ function onLoad() {
                 ctx.restore();
             }
         }
+        ctx.restore();
     }
-    function drawGhost(ctx, dead) {
+    function drawGhost(ctx, ghost) {
+        ctx.save();
+        ctx.translate(ghost.drawPos().x, ghost.drawPos().y);
+        ctx.fillStyle = ghost.color;
         // body
         ctx.beginPath();
         ctx.moveTo(-7, 7);
@@ -153,12 +234,11 @@ function onLoad() {
         ctx.lineTo(-3, 7);
         ctx.lineTo(-5, 5);
         ctx.lineTo(-7, 7);
-        if (dead) {
-            ctx.stroke();
-        }
-        else {
-            ctx.fill();
-        }
+        //if(dead) {
+        //	ctx.stroke()
+        //} else {
+        ctx.fill();
+        //}
         for (var i = -1; i < 2; i += 2) {
             ctx.save();
             ctx.translate(i * 3, -4);
@@ -170,31 +250,57 @@ function onLoad() {
             ctx.fill();
             ctx.restore();
         }
+        ctx.restore();
     }
     var openMouthAngle = Math.PI / 4;
     var mouthTimeMS = 1000;
-    function drawPlayer(ctx, timestamp) {
+    function drawPlayer(ctx, player, timestamp) {
+        ctx.save();
+        ctx.fillStyle = "yellow";
+        ctx.translate(player.drawPos().x, player.drawPos().y);
         var angle = openMouthAngle * Math.abs(1 - 2 * (timestamp / mouthTimeMS - Math.floor(timestamp / mouthTimeMS)));
         ctx.beginPath();
         ctx.arc(0, 0, 7, angle, 2 * Math.PI - angle);
         ctx.lineTo(0, 0);
         ctx.fill();
+        ctx.restore();
     }
+    function drawGame(ctx, game, timestamp) {
+        ctx.save();
+        ctx.translate(ht, ht);
+        drawLevel(ctx, game.level);
+        drawPlayer(ctx, game.player, timestamp);
+        _.forEach(game.ghosts, function (ghost) {
+            drawGhost(ctx, ghost);
+        });
+        ctx.restore();
+    }
+    document.onkeydown = function (ev) {
+        if (ev.keyCode == 37) {
+            game.playerMove("left");
+        }
+        else if (ev.keyCode == 38) {
+            game.playerMove("up");
+        }
+        else if (ev.keyCode == 39) {
+            game.playerMove("right");
+        }
+        else if (ev.keyCode == 40) {
+            game.playerMove("down");
+        }
+    };
+    var lastTime = performance.now();
     function render(timestamp) {
         if (ctx == null) {
             return;
         }
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var deltaTMS = timestamp - lastTime;
+        lastTime = timestamp;
+        game.update(deltaTMS);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.save();
         ctx.translate(0.5, 0.5);
-        ctx.translate(10, 10);
-        drawGhost(ctx, false);
-        ctx.translate(20, 0);
-        drawGhost(ctx, true);
-        ctx.translate(20, 0);
-        drawPlayer(ctx, timestamp);
-        ctx.translate(-40, 20);
-        drawLevel(ctx, game.level);
+        drawGame(ctx, game, timestamp);
         ctx.restore();
         requestAnimationFrame(render);
     }
--- frontend/src/pacman.ts
+++ frontend/src/pacman.ts
@@ -4,8 +4,14 @@ function onLoad() {
 
 	const hb = 1 // half border width
 	const ht = 4 // half tile width
-	const playerSpeed = 500
+	const playerSpeed = 5
 	const ghostSpeed = playerSpeed
+
+	const impassable = {
+		'#': true,
+		'-': true,
+		'-': true,
+	}
 
 	const levelString = `
 ############################
@@ -22,7 +28,7 @@ function onLoad() {
      #.##    B     ##.#     
      #.## ###--### ##.#     
 ######.## #I P C # ##.######
-      .   #  R   #   .      
+TODO# .   #  R   #   . #TODO
 ######.## #      # ##.######
      #.## ######## ##.#     
      #.##          ##.#     
@@ -94,6 +100,14 @@ function onLoad() {
 		mul(factor: number): Vec2 {
 			return new Vec2(this.x * factor, this.y * factor)
 		}
+
+		round(): Vec2 {
+			return new Vec2(Math.round(this.x), Math.round(this.y))
+		}
+
+		floor(): Vec2 {
+			return new Vec2(Math.floor(this.x), Math.floor(this.y))
+		}
 	}
 
 	const zero = new Vec2(0, 0)
@@ -113,7 +127,7 @@ function onLoad() {
 		dir: Vec2;
 		wantDir: Vec2;
 		progress: number; // 0-1
-		speed: number; // ms per tile
+		speed: number; // tiles per second
 
 		constructor(pos: Vec2, speed: number) {
 			this.pos = pos
@@ -123,16 +137,35 @@ function onLoad() {
 			this.progress = 0
 		}
 
-		_tryChangeDir(level: Level) {
-
+		_passable(level: Level, dir: Vec2): boolean {
+			return !impassable[level.get(this.pos.add(dir))]
 		}
 
-		update(level: Level, deltaT: number) {
-			if(this.dir.zero() && !this.wantDir.zero()) {
+		drawPos(): Vec2 {
+			return this.pos.add(this.dir.mul(this.progress)).mul(2 * ht)
+		}
+
+		update(level: Level, deltaTMS: number) {
+			if(this.dir.zero() && !this.wantDir.zero() && this._passable(level, this.wantDir)) {
 				this.dir = this.wantDir
+				this.wantDir = zero
 			}
 			if(this.dir.zero()) {
 				return
+			}
+			this.progress += deltaTMS * this.speed / 1000
+			while(this.progress >= 1) {
+				this.progress -= 1
+				this.pos = this.pos.add(this.dir)
+				if(!this.wantDir.zero() && this._passable(level, this.wantDir)) {
+					// change direction if desired & valid
+					this.dir = this.wantDir
+					this.wantDir = zero
+				} else if(!this._passable(level, this.dir)) {
+					// stop moving upon hitting wall
+					this.dir = zero
+					this.progress = 0
+				}
 			}
 		}
 	}
@@ -144,20 +177,63 @@ function onLoad() {
 	}
 
 	class Ghost extends Mover {
-		constructor(pos: Vec2) {
+		color: string;
+		constructor(pos: Vec2, color: string) {
 			super(pos, ghostSpeed)
+			this.color = color
 		}
 	}
 
 	class Game {
 		level: Level;
+		player: Player;
+		ghosts: { [name: string]: Ghost; };
 		constructor(levelString: string) {
 			this.level = new Level(levelString)
-			for (let y = 0; y < this.level.height(); y++) {
-				for (let x = 0; x < this.level.width(y); x++) {
-
+			this.ghosts = {}
+			for(let pos = new Vec2(0, 0); pos.y < this.level.height(); pos.y++) {
+				for(pos.x = 0; pos.x < this.level.width(pos.y); pos.x++) {
+					const tile = this.level.get(pos)
+					if(tile == 'M') {
+						this.player = new Player(new Vec2(pos.x, pos.y)) // copy because I'm naughty and mutate
+					} else if(tile == 'I') {
+						this.ghosts["inky"] =  new Ghost(new Vec2(pos.x, pos.y), "cyan")
+					} else if(tile == 'B') {
+						this.ghosts["blinky"] =  new Ghost(new Vec2(pos.x, pos.y), "red")
+					} else if(tile == 'P') {
+						this.ghosts["pinky"] =  new Ghost(new Vec2(pos.x, pos.y), "pink")
+					} else if(tile == 'C') {
+						this.ghosts["clyde"] =  new Ghost(new Vec2(pos.x, pos.y), "orange")
+					}
 				}
 			}
+		}
+
+		playerMove(dir: string) {
+			console.log("move " + dir)
+			const wantDir = dirs[dir]
+			if(wantDir != null) {
+				this.player.wantDir = wantDir
+			}
+		}
+
+		ghostMove(who: string, dir: string) {
+			const ghost = this.ghosts[who]
+			if(ghost == null) {
+				return
+			}
+			const wantDir = dirs[dir]
+			if(wantDir != null) {
+				ghost.wantDir = wantDir
+			}
+		}
+		
+		update(deltaTMS: number) {
+			const that = this
+			this.player.update(this.level, deltaTMS)
+			_.forEach(this.ghosts, function(ghost){
+				ghost.update(that.level, deltaTMS)
+			})
 		}
 	}
 
@@ -174,6 +250,9 @@ function onLoad() {
 	}
 
 	function drawLevel(ctx: CanvasRenderingContext2D, level: Level) {
+		ctx.save()
+		ctx.fillStyle = "blue"
+		ctx.strokeStyle = "blue"
 		const pos = new Vec2(0, 0)
 		for(; pos.y < level.height(); pos.y++) {
 			for(pos.x = 0; pos.x < level.width(pos.y); pos.x++) {
@@ -184,9 +263,13 @@ function onLoad() {
 				ctx.restore()
 			}
 		}
+		ctx.restore()
 	}
 
-	function drawGhost(ctx: CanvasRenderingContext2D, dead: boolean) {
+	function drawGhost(ctx: CanvasRenderingContext2D, ghost: Ghost) {
+		ctx.save()
+		ctx.translate(ghost.drawPos().x, ghost.drawPos().y)
+		ctx.fillStyle = ghost.color
 		// body
 		ctx.beginPath()
 		ctx.moveTo(-7, 7)
@@ -203,11 +286,11 @@ function onLoad() {
 		ctx.lineTo(-3, 7)
 		ctx.lineTo(-5, 5)
 		ctx.lineTo(-7, 7)
-		if(dead) {
-			ctx.stroke()
-		} else {
+		//if(dead) {
+		//	ctx.stroke()
+		//} else {
 			ctx.fill()
-		}
+		//}
 
 		for(let i = -1; i < 2; i += 2) {
 			ctx.save()
@@ -220,33 +303,60 @@ function onLoad() {
 			ctx.fill()
 			ctx.restore()
 		}
+		ctx.restore()
 	}
 
 	const openMouthAngle = Math.PI / 4
 	const mouthTimeMS = 1000
-	function drawPlayer(ctx: CanvasRenderingContext2D, timestamp: number) {
+	function drawPlayer(ctx: CanvasRenderingContext2D, player: Player, timestamp: number) {
+		ctx.save()
+		ctx.fillStyle = "yellow"
+		ctx.translate(player.drawPos().x, player.drawPos().y)
 		const angle = openMouthAngle * Math.abs(1 - 2 * (timestamp / mouthTimeMS - Math.floor(timestamp / mouthTimeMS)))
 		ctx.beginPath()
 		ctx.arc(0, 0, 7, angle, 2 * Math.PI - angle)
 		ctx.lineTo(0, 0)
 		ctx.fill()
+		ctx.restore()
 	}
 
+	function drawGame(ctx: CanvasRenderingContext2D, game: Game, timestamp: number) {
+		ctx.save()
+		ctx.translate(ht, ht)
+
+		drawLevel(ctx, game.level)
+		drawPlayer(ctx, game.player, timestamp)
+		_.forEach(game.ghosts, function(ghost) {
+			drawGhost(ctx, ghost)
+		})
+
+		ctx.restore()
+	}
+
+	document.onkeydown = function(ev: KeyboardEvent) {
+		if(ev.keyCode == 37) {
+			game.playerMove("left")
+		} else if(ev.keyCode == 38) {
+			game.playerMove("up")
+		} else if(ev.keyCode == 39) {
+			game.playerMove("right")
+		} else if(ev.keyCode == 40) {
+			game.playerMove("down")
+		}
+	}
+
+	let lastTime = performance.now()
 	function render(timestamp: number) {
 		if(ctx == null) {
 			return
 		}
-		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		const deltaTMS = timestamp - lastTime
+		lastTime = timestamp
+		game.update(deltaTMS)
+		ctx.fillRect(0, 0, canvas.width, canvas.height)
 		ctx.save()
 		ctx.translate(0.5, 0.5)
-		ctx.translate(10, 10)
-		drawGhost(ctx, false)
-		ctx.translate(20, 0)
-		drawGhost(ctx, true)
-		ctx.translate(20, 0)
-		drawPlayer(ctx, timestamp)
-		ctx.translate(-40, 20)
-		drawLevel(ctx, game.level)
+		drawGame(ctx, game, timestamp)
 		ctx.restore()
 		requestAnimationFrame(render)
 	}
